08778957b9b305f75600f8bc29fa1154e3acb042
juanda724 <142962462+juanda724@users.noreply.github.com>
2025-04-13 14:26:37 -0500

    Delete estructuras directory

--- estructuras/actividad.exs
+++
@@ -1,9 +0,0 @@
-defmodule Estructuras do
-  def main do
-  "\nIngrese los datos del cliente: "
-  |> Cliente.ingresar(:clientes)
-  |> Cliente.escribir_csv("clientes.csv")
-  end
-  end
-
-  Estructuras.main()
