ac5537b0b2076f256923dd364a429dcc58e1cf04
juanda724 <142962462+juanda724@users.noreply.github.com>
2025-04-13 14:24:45 -0500

    Add files via upload

---
+++ estructuras/actividad.exs
@@ -0,0 +1,9 @@
+defmodule Estructuras do
+  def main do
+  "\nIngrese los datos del cliente: "
+  |> Cliente.ingresar(:clientes)
+  |> Cliente.escribir_csv("clientes.csv")
+  end
+  end
+
+  Estructuras.main()
